782c7d0e98e2a79a4664aff6f15a4f9a80b9c1b2
Shivang0101 <shivangm910@gmail.com>
2026-09-16 17:46:34 +0530

    dynamic CORS Enabled

--- backend/app/main.py
+++ backend/app/main.py
@@ -75,6 +75,7 @@ app.add_middleware(RequestIdMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
+    allow_origin_regex=r"https?://.*",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
